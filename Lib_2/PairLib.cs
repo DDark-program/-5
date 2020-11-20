7ce2e503469f87c72618978150641040b98cc4c0
ИСП-31 <-31@LAB24-02>
2020-11-20 10:47:59 +0300

    +

--- Lib_2/PairLib.cs
+++ Lib_2/PairLib.cs
@@ -8,20 +8,38 @@ namespace Lib_2
 {
     public class Pair
     {
+
+        /// <summary>
+        /// Поля данных
+        /// </summary>
         private int _firstNumber;
         private int _secondNumber;
 
+        /// <summary>
+        /// Присваивание значения полям
+        /// </summary>
+        /// <param name="oneNumber"></param>
+        /// <param name="twoNumber"></param>
+        
         public Pair(int oneNumber, int twoNumber)
         {
             _firstNumber = oneNumber;
             _secondNumber = twoNumber;
         }
 
+        /// <summary>
+        /// Свойства первого числа
+        /// </summary>
+        
         public int First
         {
             get => _firstNumber;
             set => _firstNumber = value;
         }
+        
+        /// <summary>
+        /// Свойства второго числа
+        /// </summary>
 
         public int Second
         {
@@ -29,35 +47,26 @@ namespace Lib_2
             set => _secondNumber = value;
         }
 
+        /// <summary>
+        /// Сложение полей
+        /// </summary>
+        /// <returns></returns>
+
         public int Sum()
         {
             return _firstNumber + _secondNumber;
         }
 
+        /// <summary>
+        /// Сложение пар чисел
+        /// </summary>
+        /// <param name="firstPair"></param>
+        /// <param name="secondPair"></param>
+        /// <returns></returns>
+
         public static Pair SumPair(Pair firstPair, Pair secondPair)
         {
             return new Pair(firstPair.First + secondPair.First, firstPair.Second + secondPair.Second);
-        }
-
-        public static int SumField(int a, int b)
-        {
-            int res = a + b;
-            return res;
-        }
-        public static void SumPair(int a,int b,int c,int d,out int onepair, out int twopair)
-        {
-            onepair = a + c;
-            twopair = b + d;
-        }
-        public static int SumField(int a,int b,int c)
-        {
-            int res = a + b + c;
-            return res;
-        }
-        public static void SumPair(int a, int b, int c, int d,int f,int j, out int onepair, out int twopair)
-        {
-            onepair = a + c + f;
-            twopair = b + d + j;
         }
     }
 }
